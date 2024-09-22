424793dbf596714c79ef0f4c47db40d9b5c6a45d
Tarikko-ScetayhChan <weychon_lyon@outlook.com>
2024-09-22 12:17:47 +0800

    修改dollopium

--- dollopium/js/countdown.js
+++
@@ -1,7 +0,0 @@
-var targetTime = new Date();
-targetTime.setMinutes(targetTime.getMinutes() + totalTime);
-
-var minEle = document.getElementById("min");
-var secEle = document.getElementById("sec");
-
-setInterval(renderCountDown, 100);
